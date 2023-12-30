93cc1c01bcc70d32d4f737db819b61f5b8dd160c
WeirdPtr <weirdptr@weirdptr.com>
2023-12-30 16:32:47 +0100

    feat(models): add missing `bid` response field definition (#13)

--- src/models/structs/api/response/deposit/bid.rs
+++ src/models/structs/api/response/deposit/bid.rs
@@ -11,26 +11,26 @@ pub struct BidResponse {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct AuctionData {
-    pub id: i64,
-    pub app_id: i64,
-    pub auction_highest_bid: i64,
-    pub auction_highest_bidder: i64,
-    pub auction_number_of_bids: i64,
-    pub auction_ends_at: i64,
+    pub id: u64,
+    pub app_id: Option<u64>,
+    pub auction_highest_bid: u64,
+    pub auction_highest_bidder: u64,
+    pub auction_number_of_bids: u64,
+    pub auction_ends_at: u64,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Invoice {
-    pub user_id: i64,
-    pub status: i64,
-    pub processor_id: i64,
-    pub currency_id: i64,
-    pub amount_coins: i64,
+    pub user_id: u64,
+    pub status: u64,
+    pub processor_id: u64,
+    pub currency_id: u64,
+    pub amount_coins: u64,
     pub metadata: Metadata,
     pub ip: String,
-    pub updated_at: i64,
-    pub created_at: i64,
-    pub id: i64,
+    pub updated_at: u64,
+    pub created_at: u64,
+    pub id: u64,
     pub processor_ref: String,
     pub status_name: String,
     pub processor_name: String,
@@ -41,5 +41,5 @@ pub struct Invoice {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Metadata {
-    pub deposit_id: i64,
+    pub deposit_id: u64,
 }
